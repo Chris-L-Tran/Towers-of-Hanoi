9371eebd003102e96e0ce73b7f18b354da9d5261
Christopher Tran <christopher.long.tran@gmail.com>
2017-12-11 16:24:26 -0500

    Added Timer to scoreboard

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 console.log("yay")
 
+function startTimer(){
+    var time = 0;
+    setInterval(function(){
+        time++;
+    $("#score .timeUsed").text(time)
+    }, 1000)
+}
+
 function test(){
+    startTimer()
+
     var counter = parseInt($("#moves .moveNum").text())
     counter++
     $("#moves .moveNum").text(counter)
+    
+
 }
 
 $("#test").click(test)
